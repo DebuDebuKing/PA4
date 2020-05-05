473a10e4c9e5c6af2f1c8ae3516672f1149bfb64
DebuDebuKing <48768830+DebuDebuKing@users.noreply.github.com>
2020-05-05 12:56:12 +0800

    Rotate

---
+++ Rotate/Assets/spin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    [SerializeField] float spd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate( Vector3.right * spd * Time.deltaTime);
+    }
+}
